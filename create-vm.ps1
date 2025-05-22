43bd5fcfe9d1de72727942b23ba9a19310ab1c7f
ShyamSatasiya <88236412+ShyamSatasiya@users.noreply.github.com>
2025-05-22 16:21:24 -0400

    Update create-vm.ps1

--- create-vm.ps1
+++ create-vm.ps1
@@ -4,14 +4,24 @@ $vmName = "MyVM"
 
 New-AzResourceGroup -Name $resourceGroup -Location $location -Force
 
-$cred = Get-Credential
-New-AzVm `
-  -ResourceGroupName $resourceGroup `
-  -Name $vmName `
-  -Location $location `
-  -VirtualNetworkName "$vmName-VNet" `
-  -SubnetName "$vmName-Subnet" `
-  -SecurityGroupName "$vmName-NSG" `
-  -PublicIpAddressName "$vmName-PIP" `
-  -Credential $cred
+# Read username and password from environment variables (set by GitHub Actions)
+$vmUsername = $env:VM_USERNAME
+$vmPasswordPlain = $env:VM_PASSWORD
+
+# Convert plain password to secure string
+$securePassword = ConvertTo-SecureString $vmPasswordPlain -AsPlainText -Force
+
+# Create PSCredential object
+$cred = New-Object System.Management.Automation.PSCredential ($vmUsername, $securePassword)
+
+# Now use $cred in your VM creation command:
+New-AzVM -ResourceGroupName $resourceGroup `
+         -Name $vmName `
+         -Location $location `
+         -VirtualNetworkName $vnetName `
+         -SubnetName $subnetName `
+         -SecurityGroupName $nsgName `
+         -PublicIpAddressName $pipName `
+         -OpenPorts 3389,80 `
+         -Credential $cred
 
